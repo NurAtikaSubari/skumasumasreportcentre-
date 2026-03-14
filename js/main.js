5b3df24bf13dd5d7174392914f965928acab6e4a
NurAtikaSubari <90764747+NurAtikaSubari@users.noreply.github.com>
2026-03-14 22:20:41 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // ================================
 
 // Paste your Web App URL here (from Apps Script deployment)
-const SHEET_URL = "https://script.google.com/macros/s/AKfycbz6IcRvNPYc6lYM6PJvpUk0774kN7CpV0tortnH9ZG_EzEMwpfhYr0h-PI83xwREjs7dQ/exec";
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbygzmRHlrTyMRlejqdlLk93BQ7jMb9jopWTFE9mCYiGccHuuPorhFXkN1VZ5GNOxleoDw/exec";
 
 // Universal function to send row data to Google Sheets
 async function sendToGoogleSheet(sheetName, row) {
